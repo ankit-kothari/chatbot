d775fccde39ebc35c291f25e2ea677001efeb48c
ankit-kothari <ankit256@gmail.com>
2020-02-05 15:47:07 -0500

    added loop to  python  file

--- dockerp.py
+++ dockerp.py
@@ -3,4 +3,36 @@ import pandas as pd
 data = pd.read_csv('./dialog_talk_agent.csv', engine='python', encoding='utf8')
 for i in data.iterrows():
   print(i)
+
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+for i in data.iterrows():
+  print(i)
+
 print(data)
